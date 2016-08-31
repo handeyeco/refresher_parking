f944d6b52a312bf6aa615cad20d7a33b9e78d5cd
Matthew Curtis <matthew@matthewbryancurtis.com>
2016-08-31 09:29:08 -0700

    Finish next-slide button

--- main.js
+++ main.js
@@ -1,38 +1,63 @@
-$(function(){
+$(function() {
 
-    var pagePosition = 0,
-        $sections = $('section'),
-        pageMaxPosition = $sections.length - 1;
+  var $sections = $('section'),
+    $nextSlide = $('#next-slide'),
+    pagePosition = 0,
+    previousPage,
+    pageMaxPosition = $sections.length - 1;
 
-    //Map the sections with unique identifier
-    $sections.each(function(idx, elem) { $(elem).data("pos", idx); });
+  //Map the sections with unique identifier
+  $sections.each(function(idx, elem) {
+    $(elem).data("pos", idx);
+  });
 
-    //Call updatePosition on scroll
-    $(window).bind('scroll', updatePosition);
+  //Call updatePosition on scroll
+  $(window).bind('scroll', updatePosition);
 
-    //Move on click
-    $('#next-slide').click(function(e){
-        if (pagePosition + 1 <= pageMaxPosition) {
-            pagePosition++;
+  //Move on click
+  $($nextSlide).click(nextSlideClick);
 
-            $('html, body').stop().animate({
-                  scrollTop: $sections.eq(pagePosition).offset().top
-            }, 500);
-        }
+  function nextSlideClick() {
+    if (pagePosition + 1 <= pageMaxPosition) {
+      pagePosition++;
+
+      $('html, body').stop().animate({
+        scrollTop: $sections.eq(pagePosition).offset().top
+      }, 500);
+    }
+  }
+
+  //Update position
+  function updatePosition() {
+    var fromTop = $(this).scrollTop(),
+      $curr = null;
+
+    $sections.each(function(idx, elem) {
+      if ($(elem).offset().top < fromTop + 30) {
+        $curr = $(elem);
+      }
     });
 
-    //Update position
-    function updatePosition(){
-       var fromTop = $(this).scrollTop();
-       var $curr = null;
+    if ($curr && pagePosition !== $curr.data('pos')) {
+      previousPage = pagePosition;
+      pagePosition = $curr.data('pos');
 
-       $sections.each(function(idx, elem){
-           if ( $(elem).offset().top < fromTop + 30 ) { $curr = $(elem); }
-       });
-
-       if ($curr && pagePosition !== $curr.data('pos')) {
-           pagePosition = $curr.data('pos');
-       }
+      toggleButton();
     }
+  }
+
+  function toggleButton() {
+    // Hide next button if at the bottom of page
+    if (pagePosition === pageMaxPosition && previousPage !== pageMaxPosition) {
+      $($nextSlide).animate({
+        bottom: -$(this).outerHeight()
+      }, 800);
+      // Show next button if leaving the bottom of page
+    } else if (pagePosition !== pageMaxPosition && previousPage === pageMaxPosition) {
+      $($nextSlide).animate({
+        bottom: 0
+      }, 800);
+    }
+  }
 
 });

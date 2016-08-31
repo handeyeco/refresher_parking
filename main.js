4903bd2d016f9b58afaeb01eb5ad92217583e19b
Matthew Curtis <matthew@matthewbryancurtis.com>
2016-08-30 20:13:34 -0700

    Add scroll responsive page button

--- main.js
+++ main.js
@@ -1,13 +1,38 @@
-$(function () {
+$(function(){
 
-  var onSlide = 0;
+    var pagePosition = 0,
+        $sections = $('section'),
+        pageMaxPosition = $sections.length - 1;
 
-  $('#next-slide').click(function () {
-    onSlide++;
+    //Map the sections with unique identifier
+    $sections.each(function(idx, elem) { $(elem).data("pos", idx); });
 
-    $("html, body").stop().animate({
-      "scrollTop": $('#slide-' + onSlide).offset().top
-    }, 900, "swing");
-  })
+    //Call updatePosition on scroll
+    $(window).bind('scroll', updatePosition);
 
-})
+    //Move on click
+    $('#next-slide').click(function(e){
+        if (pagePosition + 1 <= pageMaxPosition) {
+            pagePosition++;
+
+            $('html, body').stop().animate({
+                  scrollTop: $sections.eq(pagePosition).offset().top
+            }, 500);
+        }
+    });
+
+    //Update position
+    function updatePosition(){
+       var fromTop = $(this).scrollTop();
+       var $curr = null;
+
+       $sections.each(function(idx, elem){
+           if ( $(elem).offset().top < fromTop + 30 ) { $curr = $(elem); }
+       });
+
+       if ($curr && pagePosition !== $curr.data('pos')) {
+           pagePosition = $curr.data('pos');
+       }
+    }
+
+});
